38470fd8cfea54c773be685d3d77e75f1feb21cd
David Diaz <daviddiazlds1221@gmail.com>
2021-11-22 18:28:10 -0500

    Configuracion del UsuarioController en el controlador para obtener y guardar usuarios.

--- src/main/java/com/crud/demo/controllers/UsuarioController.java
+++ src/main/java/com/crud/demo/controllers/UsuarioController.java
@@ -1,4 +1,25 @@
 package com.crud.demo.controllers;
 
+import com.crud.demo.models.UsuarioModel;
+import com.crud.demo.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
 }
