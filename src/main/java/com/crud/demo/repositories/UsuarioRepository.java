a908f39f71bd622de04b8f2cefef82e7f976c7fe
David Diaz <daviddiazlds1221@gmail.com>
2021-11-22 18:46:21 -0500

    Se agregaron los metodos de buscar usuarios por prioridad, eliminar usuarios y modificar usuarios.

--- src/main/java/com/crud/demo/repositories/UsuarioRepository.java
+++ src/main/java/com/crud/demo/repositories/UsuarioRepository.java
@@ -5,9 +5,11 @@ import com.crud.demo.models.UsuarioModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
 
-
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
 
 }
--- src/main/java/com/crud/demo/services/UsuarioService.java
+++ src/main/java/com/crud/demo/services/UsuarioService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -20,6 +21,23 @@ public class UsuarioService {
 
     public UsuarioModel guardarUsuario(UsuarioModel usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    public Optional<UsuarioModel> obtenerPorId(Long id){
+        return usuarioRepository.findById(id);
+    }
+
+    public ArrayList<UsuarioModel>  obtenerPorPrioridad(Integer prioridad) {
+        return usuarioRepository.findByPrioridad(prioridad);
+    }
+
+    public boolean eliminarUsuario(Long id) {
+        try{
+            usuarioRepository.deleteById(id);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
     }
 
 }
